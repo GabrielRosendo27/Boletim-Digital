b6ad3cedfc6f286939268faab7bce892bd38a13f
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2023-04-24 15:33:26 -0300

    boletim-digital-c1

---
+++ index.js
@@ -0,0 +1,8 @@
+const botao = document.querySelector(".botao");
+console.log(botao);
+
+botao.addEventListener("click", function () {
+  // código
+  const nomeAluno = document.querySelector("#nome-aluno").value;
+  console.log(nomeAluno);
+});
