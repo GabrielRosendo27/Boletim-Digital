16038ea0327a65d49099349d0071a865233210df
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2023-04-25 16:57:34 -0300

    notas-c2

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const botao = document.querySelector(".botao");
-console.log(botao);
+const info1 = document.querySelector(".info-1");
+const info2 = document.querySelector(".info-2");
 
 botao.addEventListener("click", function () {
-  // código
+  // data from info1
   const nomeAluno = document.querySelector("#nome-aluno").value;
-  console.log(nomeAluno);
+  const nomeEscola = document.querySelector("#nome-escola").value;
+  const nomeTurma = document.querySelector("#nome-turma").value;
+  const anoLetivo = document.querySelector("#ano-letivo").value;
+  // --
+  info1.style.display = "none";
+  info2.style.display = "grid";
+  // --
+});
+
+function materias() {
+  const arte = document.querySelector("#arte");
+  const biologia = document.querySelector("#biologia");
+  const sociologia = document.querySelector("#sociologia");
+  const historia = document.querySelector("#historia");
+  const geografia = document.querySelector("#geografia");
+  const portugues = document.querySelector("#portugues");
+  const quimica = document.querySelector("#quimica");
+  const matematica = document.querySelector("#matematica");
+  const ingles = document.querySelector("#ingles");
+  const filosofia = document.querySelector("#filosofia");
+}
+
+//
+const secondButton = document.querySelector(".second-button");
+secondButton.addEventListener("click", function () {
+  materias();
+  arte.style.display = "none";
+  biologia.style.display = "grid";
 });
